b68a480f20913d7b4060e68cfb3ae12a429c0cd4
tt4623 <sltebr33zy@gmail.com>
2021-03-03 19:09:06 -0600

    added components Header and buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Table from './components/Table/index';
+import Btn from './components/Button/index';
+import Header from './components/Header/index';
+import axios from 'axios';
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=10&nat=us,ca')
+      .then(res => setEmployees(res.data.results))
+  }, []);
+
+  function sortAge() {
+    const sorted = employees.sort((a, b) => a.dob.age - b.dob.age);
+    setEmployees([...sorted]);
+    // maybe clean up
+  }
+
+  function filterCountry(country) {
+    const filtered = employees.filter(employee => employee.location.country === country)
+    setEmployees([...filtered]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Table employees={employees}/>
+      <Btn onClick={() => sortAge()}>Sort Age</Btn>
+      <Btn onClick={() => filterCountry("United States")}>From US</Btn>
     </div>
-  );
+  )
 }
 
 export default App;
