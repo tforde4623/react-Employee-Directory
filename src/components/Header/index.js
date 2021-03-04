b04c6abbbd60c9501a7fbfdb5c5e1a2b751b3a0c
tt4623 <sltebr33zy@gmail.com>
2021-03-03 19:06:23 -0600

    created sample Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Employee Tracker</h1>
+      <p className="lead">... with React</p>
+    </div>
+  )
+}
+
+export default Header;
